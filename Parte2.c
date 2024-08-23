61f5daf711c454ecba1a31dff56a5a85ed27cdf9
amb231143 <amb231143@uvg.edu.gt>
2024-08-23 12:42:23 -0600

    Parte 2 - terminada
    Se termino de realizar el código del inciso 2, y se comento el código.

--- Parte2.c
+++ Parte2.c
@@ -1,21 +1,40 @@
-#include <stdio.h>
-#include <omp.h>
-
-#define N 1000000
+/*
+* ------------------------------------------------------------
+* Parte2.c
+* ------------------------------------------------------------
+* UNIVERSIDAD DEL VALLE DE GUATEMALA
+* CC3086 - Programacion de Microprocesadores
+* Ver.: 1.1 2024/24/08
+* Autor: Dulce Ambrosio - 231143
+*
+* References:
+*Aprende OpenMP en paralelo.dev, s.f
+*
+* Descripcion:
+* Calcula la suma de los primeros N numeros (N=1000000) usando
+* el ciclo for parallel y reduction con + para la acumulacion
+* de la suma.
+* ------------------------------------------------------------
+*/
+#include <stdio.h> // Biblioteca para la salida y entrada de datos
+#include <omp.h> // Biblioteca para el uso de la programacion paralela
 
 int main(){
 
-    #pragma omp parallel for reduction(+ : sum)
-        int i;
-        int sum = 0;
+    int N = 1000000; // La cantidad de numeros que se van a sumar
+    int suma = 0; // Variable que almacena la suma
 
-        for(i=0; i < N; i++){
-            sum += N;
-        }
+        double start_time, end_time; // se declaran las variable para el tiempo de inicio y fin de la ejecucion
 
-        printf ("El valor de la suma final es: %d\n", sum);
-        printf ("El tiempo transcurrido fue: %d\n", omp_get_wtime());
+        start_time = omp_get_wtime(); // se inicia a medir el tiempo de la ejecucion
+        #pragma omp parallel for reduction(+:suma) // se inicia el bucle for en paralelo y con la clausula reduction
+            for(int i=1; i<=N ; i++){ // se inicia el ciclo para realizar el suma de los valores de N
+                suma = suma + i;
+            } 
+        end_time = omp_get_wtime(); //se deja de medir el tiempo de ejecucion y se captura el valor
 
-        return 0 ; 
-    
+        printf("La suma total es: %d\n", suma); // se imprime el valor de la suma
+        printf("El tiempo de ejecucion es: %f segundos.\n", end_time - start_time); // se imprime el tiempo de ejecucion
+
+    return 0;
 }
